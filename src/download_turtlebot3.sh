b39bdb8aacf6790dec34c804ada700bdc0775d55
Samu PC <sbob1@gcloud.ua.es>
2023-03-13 16:10:16 +0100

    Script to download turtlebot3

---
+++ src/download_turtlebot3.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+
+git clone -b noetic-devel https://github.com/ROBOTIS-GIT/turtlebot3.git
+git clone -b noetic-devel https://github.com/ROBOTIS-GIT/turtlebot3_msgs.git
+git clone -b noetic-devel https://github.com/ROBOTIS-GIT/turtlebot3_simulations.git

8ae2c953e207249b0309625c1e2bf7871c31a539
Samuel <samuoliva9@gmail.com>
2023-03-14 23:19:28 +0100

    Checking going back of checkpoints

--- src/all_listeners/scripts/test.py
+++ src/all_listeners/scripts/test.py
@@ -79,3 +79,24 @@ print([v.valor for v in valores])
 [v.incrementar() for v in valores]
 
 print([v.valor for v in valores])
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+print(f'{bcolors.HEADER}HEADER {bcolors.ENDC}')
+print(f'{bcolors.OKBLUE}OKBLUE {bcolors.ENDC}')
+print(f'{bcolors.OKCYAN}OKCYAN {bcolors.ENDC}')
+print(f'{bcolors.OKGREEN}OKGREEN{bcolors.ENDC}')
+print(f'{bcolors.WARNING}WARNING{bcolors.ENDC}')
+print(f'{bcolors.FAIL}FAIL{bcolors.ENDC}')
+print(f'{bcolors.ENDC}ENDC{bcolors.ENDC}')
+print(f'{bcolors.BOLD}BOLD{bcolors.ENDC}')
+print(f'{bcolors.UNDERLINE}UNDERLINE{bcolors.ENDC}')
+print(f'{bcolors.HEADER}{bcolors.BOLD}{bcolors.UNDERLINE}{bcolors.WARNING}EVERYTHIN{bcolors.ENDC}')

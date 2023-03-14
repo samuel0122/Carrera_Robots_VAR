016aa967a40ecf9ae0e33cabf9b23de88e94e9fc
Samu PC <sbob1@gcloud.ua.es>
2023-03-14 14:03:30 +0100

    Checkpoints & training

--- src/all_listeners/scripts/robot_AI_training.py
+++ src/all_listeners/scripts/robot_AI_training.py
@@ -40,8 +40,11 @@ LATER_WALL_CRASH_PATIENTE = 15
 LASER_MAX_DISTANCE = 5.0
 LASER_MIN_DISTANCE = 0.2
 INITIAL_ROBOT_X, INITIAL_ROBOT_Y, INITIAL_ROBOT_Z = -6.5, 8.5, 0.2
+INITIAL_ROBOT_X, INITIAL_ROBOT_Y, INITIAL_ROBOT_Z = 0, -9, 0.2
 STATES_SAVE_DIRECTORY = '/home/samuel/Carrera_Robots_VAR/src/all_listeners/states/'
 STATESLIST_SAVE_DIRECTORY = '/home/samuel/Carrera_Robots_VAR/src/all_listeners/statesLists/'
+
+INITIAL_ROBOT_X = 2
 
 SAVE_STATE_EVERY_GENERATIONS = 15
 
@@ -93,6 +96,8 @@ class Wander:
         self.robotCrashedEvent = None
         self.timeStarted = None
         self.timeCrashed = None
+        
+        self.checkPoint = 0
 
         # Checks of loops
         self.lastTimeChecked = None
@@ -124,6 +129,8 @@ class Wander:
     def resetData(self):
         # Variables to know if the robot is alive
         self.robotCrashedEvent = threading.Event()
+        
+        self.checkPoint = 5
         
         # Checks of loops
         self.last_x_checked, self.last_y_checked = INITIAL_ROBOT_X, INITIAL_ROBOT_Y
@@ -164,7 +171,7 @@ class Wander:
         spawnMsg.pose.position.x, spawnMsg.pose.position.y, spawnMsg.pose.position.z = INITIAL_ROBOT_X, INITIAL_ROBOT_Y, INITIAL_ROBOT_Z
 
         # Spawn orientation
-        spawnMsg.pose.orientation.x, spawnMsg.pose.orientation.y, spawnMsg.pose.orientation.z = 0, 0, 0
+        spawnMsg.pose.orientation.x, spawnMsg.pose.orientation.y, spawnMsg.pose.orientation.z = 0, 0, 180
 
         # Send the message and close the publisher
         time.sleep(0.5)
@@ -220,6 +227,8 @@ class Wander:
                    self.scannerData[0] < LASER_MIN_DISTANCE and abs(self.scannerData[0] - self.previousScannnerData[0]) < 0.05 
                 or self.scannerData[1] < LASER_MIN_DISTANCE and abs(self.scannerData[1] - self.previousScannnerData[1]) < 0.05
                 or self.scannerData[2] < LASER_MIN_DISTANCE and abs(self.scannerData[2] - self.previousScannnerData[2]) < 0.05
+                #or self.scannerData[3] < LASER_MIN_DISTANCE and abs(self.scannerData[3] - self.previousScannnerData[3]) < 0.05
+                #or self.scannerData[4] < LASER_MIN_DISTANCE and abs(self.scannerData[4] - self.previousScannnerData[4]) < 0.05
                 )
 
     def getScanValues(self, msg: LaserScan):
@@ -258,14 +267,70 @@ class Wander:
         if not self.robotCrashedEvent.is_set():
             self.command_pub.publish(msg)
 
+
     """
         Robot odometry callback
     """
+    def getCheckPoints(self):
+        return self.checkPoint
+    
+    def checkPoints(self, newX, newY):
+        if self.checkPoint < 1:
+            if newY < 7 and newX < 9 and newX > 10:
+                self.checkPoint = -1
+            else:
+                self.checkPoint = 0
+            
+        if self.checkPoint < 1:   # Checkpoint 1
+            if newX > 5 and newX < 10 and newY < 5.95:
+                self.checkPoint = 1
+        elif self.checkPoint < 2: # Checkpoint 2
+            if newX > 5 and newX < 10 and newY < 3.95:
+                self.checkPoint = 2
+        elif self.checkPoint < 3: # Checkpoint 3
+            if newX > 5 and newX < 10 and newY < 1.95:
+                self.checkPoint = 3
+        elif self.checkPoint < 4: # Checkpoint 4
+            if newX > 8 and newX < 10 and newY < -3:
+                self.checkPoint = 4
+        elif self.checkPoint < 5: # Checkpoint 5
+            if newX < 3 and newY > -10 and newY < -8:
+                self.checkPoint = 5
+        elif self.checkPoint < 6: # Checkpoint 6
+            if newX < -7 and newY > -10 and newY < -8:
+                self.checkPoint = 6
+        elif self.checkPoint < 7: # Checkpoint 7
+            if newY > -7 and newX > -10 and newX < -8:
+                self.checkPoint = 7
+        elif self.checkPoint < 8: # Checkpoint 8
+            if newX > -6 and newY > -3 and newY < -1:
+                self.checkPoint = 8
+        elif self.checkPoint < 9: # Checkpoint 9
+            if newX > -2 and newY > -5 and newY < -3:
+                self.checkPoint = 9
+        elif self.checkPoint < 10: # Checkpoint 10
+            if newY > 0 and newX > 1 and newX < 3:
+                self.checkPoint = 10
+        elif self.checkPoint < 11: # Checkpoint 11
+            if newX < 0.5 and newY > 1 and newY < 3:
+                self.checkPoint = 11
+        elif self.checkPoint < 12:  # Checkpoint 12
+            if newX < -7 and newY > 1 and newY < 3:
+                self.checkPoint = 12
+        elif self.checkPoint < 13: # Checkpoint 13
+            if newY > 4 and newX > -10 and newX < -8:
+                self.checkPoint = 13
+        elif self.checkPoint < 14: # Checkpoint 14
+            if newX > -8 and newY > 7 and newY < 10:
+                self.checkPoint = 14
+        
     def odom_callback(self, msg: Odometry):
         
         # Keep odometry data     
         newX = msg.pose.pose.position.x
         newY = msg.pose.pose.position.y
+        
+        self.checkPoints(newX=newX, newY=newY)
         
         if time.time() > self.lastTimeChecked + CHECK_EVERY_SECONDS:
             #Its time to check again
@@ -336,7 +401,13 @@ class Wander:
 
 def compare_robots(robot1: Wander, robot2: Wander):
 
-    DISTANCE_DIFFERENCE = 0.2
+    checkPoint1 = robot1.getCheckPoints()
+    checkPoint2 = robot2.getCheckPoints()
+    
+    if checkPoint1 != checkPoint2:
+        return -1 if checkPoint1 > checkPoint2 else 1
+    
+    DISTANCE_DIFFERENCE = 1
 
     robot1X, robot1Y = robot1.getMovedDistances()
     robot2X, robot2Y = robot2.getMovedDistances()
@@ -348,24 +419,28 @@ def compare_robots(robot1: Wander, robot2: Wander):
             robot1Time = robot1.getTimeAlive()
             robot2Time = robot2.getTimeAlive()
 
-            if robot1Time < 20 and robot2Time < 20:
-                # If both times are low (at beggining) put first the lowest
-                return -1 if robot1Time < robot2Time else 1
-            else:
-                # If time are long, put first the one who survived more time
-                return -1 if robot1Time > robot2Time else 1
+            # if robot1Time < 10 and robot2Time < 10:
+            #     # If both times are low (at beggining) put first the lowest
+            #     return -1 if robot1Time < robot2Time else 1
+            # else:
+            # If time are long, put first the one who survived more time
+            return -1 if robot1Time > robot2Time else 1
         else:
             # Same X distance, different Y distance
             # Put first the robot who runned more Y distance
             return -1 if robot1Y > robot2Y else 1
     else:
         # Different X distances
-        # Put first the robot who runned more X distance
+        if abs(robot1Y - robot2Y) > DISTANCE_DIFFERENCE:
+            # If different X and different Y, look who got further X-wise and Y-wise
+            return -1 if (robot1X - robot2X) + (robot1Y - robot2Y) > 0 else 1
+        
+        # If same y, put first the robot who runned more X distance
         return -1 if robot1X > robot2X else 1
 
 
 class Population:
-    MUTATION_RANGE = 0.01
+    MUTATION_RANGE = 0.02
     MUTATION_RATE  = 0.1
     CROSSOVER_RATE = 0.5
     GenVersion = 0
@@ -445,6 +520,8 @@ class Population:
         
         # Sort the robots
         self.generation.sort(key=cmp_to_key(compare_robots))
+        
+        print([r.getTimeAlive() for  r in self.generation])
 
         # Keep top 3
         robot1Weights = self.generation[0].getRobotWeights()
@@ -475,12 +552,16 @@ if __name__ == '__main__':
     rospy.init_node('AI_robot_controlled')
 
     pop = Population()
-    pop.loadState('firstGen.txt')
+    pop.loadState('trainingDefault.txt')
     
-    while True:
+    for _ in range(5):
         
         for _ in range(SAVE_STATE_EVERY_GENERATIONS):
             pop.simulateGeneration()
             pop.nextGen()
             
         pop.saveState(f'Gen{pop.GenVersion}.txt')
+
+    pop.saveState(f'trainingDefault.txt')
+        
+        
